2271b00ec9df95a8fe26a7afab00e4b953092066
nikbai <nikolabaic@outlook.com>
2023-03-13 02:27:30 -0400

    Update models.py
    change var name

--- app/models.py
+++ app/models.py
@@ -143,7 +143,7 @@ class User(UserMixin, PaginatedAPIMixin, db.Model):
     def password(self, password):
         self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
+    def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
     # definitions for MFA
